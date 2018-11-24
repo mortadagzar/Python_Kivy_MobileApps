e9e0dca5c5d0958e538b4831184d693fafdb5f31
mortada gzar <mortadagzar@gmail.com>
2018-11-24 15:36:31 -0800

    Create label.py

---
+++ label.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='hello mova')
+guuu
+
+
+gcgvh
+
+if __name__== '__main__' :
+        MyApp().run()
