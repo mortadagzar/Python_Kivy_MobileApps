5cabc1ea74f119743153fa313821fbad99beb607
mortada gzar <mortadagzar@gmail.com>
2018-11-25 12:15:45 -0800

    Update label.py

--- label.py
+++ label.py
@@ -4,10 +4,8 @@ from kivy.uix.label import Label
 class MyApp(App):
     def build(self):
         return Label(text='hello mova')
-guuu
 
 
-gcgvh
 
 if __name__== '__main__' :
         MyApp().run()
