5374b83af63aef5e2056672f9fa00c3f667f022a
mortada gzar <mortadagzar@gmail.com>
2018-11-26 09:44:21 -0800

    rt

--- main.py
+++ main.py
@@ -1,62 +1,28 @@
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.core.window import Window
 from kivy.uix.widget import Widget
-from kivy.resources import resource_find
-from kivy.graphics.transformation import Matrix
-from kivy.graphics.opengl import *
-from kivy.graphics import *
-from objloader import ObjFile
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
 
-class Renderer(Widget):
-    def __init__(self, **kwargs):
-        self.canvas = RenderContext(compute_normal_mat=True)
-        self.canvas.shader.source = resource_find('simple.glsl')
-        self.scene = ObjFile(resource_find("monkey.obj"))
-        super(Renderer, self).__init__(**kwargs)
-        with self.canvas:
-            self.cb = Callback(self.setup_gl_context)
-            PushMatrix()
-            self.setup_scene()
-            PopMatrix()
-            self.cb = Callback(self.reset_gl_context)
-        Clock.schedule_interval(self.update_glsl, 1 / 60.)
 
-    def setup_gl_context(self, *args):
-        glEnable(GL_DEPTH_TEST)
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
 
-    def reset_gl_context(self, *args):
-        glDisable(GL_DEPTH_TEST)
-
-    def update_glsl(self, delta):
-        asp = self.width / float(self.height)
-        proj = Matrix().view_clip(-asp, asp, -1, 1, 1, 100, 1)
-        self.canvas['projection_mat'] = proj
-        self.canvas['diffuse_light'] = (1.0, 1.0, 0.8)
-        self.canvas['ambient_light'] = (0.1, 0.1, 0.1)
-        self.rot.angle += delta * 100
-
-    def setup_scene(self):
-        Color(1, 1, 1, 1)
-        PushMatrix()
-        Translate(0, 0, -3)
-        self.rot = Rotate(1, 0, 1, 0)
-        m = list(self.scene.objects.values())[0]
-        UpdateNormalMatrix()
-        self.mesh = Mesh(
-            vertices=m.vertices,
-            indices=m.indices,
-            fmt=m.vertex_format,
-            mode='triangles',
-        )
-        PopMatrix()
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
-class RendererApp(App):
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
     def build(self):
-        return Renderer()
+        return PongGame()
 
 
-if __name__ == "__main__":
-    RendererApp().run()
+if __name__ == '__main__':
+    PongApp().run()
+
