df307fae0e0fb783e442c1e388896de6ae49c32b
mortada gzar <mortadagzar@gmail.com>
2018-11-27 13:41:06 -0800

    Update label.py

--- label.py
+++ label.py
@@ -1,11 +1,1 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class MyApp(App):
-    def build(self):
-        return Label(text='hello mova')
-
-
-
-if __name__== '__main__' :
-        MyApp().run()
+cvhj
