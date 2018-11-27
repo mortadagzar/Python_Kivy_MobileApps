950b9f45b6727a39d712c85b37be0759ce397972
mortada gzar <mortadagzar@gmail.com>
2018-11-26 16:14:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
-from kivy.vector import Vector
+i=5
+while True:
+    x=input("your name please: ")
+    print("i want to suck you dick",x)
+    i += 1
+    x=input("your name please: ")
+    if i>= 7:
+        break
 
 
-
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
-
-
-class PongGame(Widget):
-    pass
-
-
-class PongApp(App):
-    def build(self):
-        return PongGame()
-
-
-if __name__ == '__main__':
-    PongApp().run()
-
+    
+    
