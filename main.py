d006b7ea55150bedfa632d7d2d2ebbff9c0ea4fe
mortada gzar <mortadagzar@gmail.com>
2018-12-02 21:31:22 -0800

    vh

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("htji")
+print ("**********************************************************")
+class Circle :
+    def  __init__(self,width, height):
+        self.width=width
+        self.height=height
+        x=width*height
+        
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2* (self.width +self.height)
+    
+r1=Circle(10,20)
+print("the area of this is : ",r1.area())
+print("the perimeter(of this is : ",r1.perimeter())
+print(str(r1))
