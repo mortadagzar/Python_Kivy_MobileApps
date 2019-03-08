d941a8d2da00a2420a47b07224e3ed876698ea23
mortada gzar <mortadagzar@gmail.com>
2019-03-07 21:17:07 -0800

    d

---
+++ edv.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> python
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    python
+NameError: name 'python' is not defined
+>>> 
